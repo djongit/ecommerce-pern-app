e32cbedcaef5988c8e8181efb96ac15dcf55bf79
unknown <djongit@hotmail.com>
2024-02-14 12:10:23 +0000

    Register slice and register store created

--- funky-client/src/features/index/store.js
+++ funky-client/src/features/index/store.js
@@ -1,1 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import registerReducer from '../register/RegisterSlice';
+const store = configureStore({
+    reducer: {
+        register: registerReducer,
+        
+    }
+})
--- funky-client/src/features/register/RegisterSlice.js
+++ funky-client/src/features/register/RegisterSlice.js
@@ -1,5 +1,5 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-
+import { createSlice } from '@reduxjs/toolkit';
+import { registerUserActions } from './RegisterActions';
 const initialState = {
     isLoading: false,
     isAuthenticated: false,
@@ -12,6 +12,22 @@ const registerSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: {
+        [registerUserActions.pending]: (state, action) => {
+            state.isLoading = true,
+            state.error = false,
+            isAuthenticated = false
+        },
+        [registerUserActions.rejected]: (state, action) => {
+            state.isLoading = false,
+            state.error = action.error.message
+        },
+        [registerUserActions.fulfilled]: (state, action) => {
+            state.isLoading = false,
+            state.error = action.error,
 
+        }
     }
 });
+
+
+export default registerSlice.reducer;
